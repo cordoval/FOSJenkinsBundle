bd0806a7d9f5d576f68a6342117b802ca8a73de8
Hugo Hamon <hugo.hamon@sensio.com>
2011-04-06 23:42:23 +0200

    [Tests] added 100% unit test code coverage for the CommitHistory class.

---
+++ Tests/BuildAnalysis/CommitHistoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the FOSJenkins package.
+ *
+ * (c) Hugo Hamon <hugo.hamon@sensio.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\Bundle\JenkinsBundle\Tests\BuildAnalysis;
+
+use FOS\Bundle\JenkinsBundle\BuildAnalysis\CommitHistory;
+
+class CommitHistoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \SimpleXmlElement
+     */
+    private $xml;
+
+    protected function setup()
+    {
+        $this->xml = new \SimpleXmlElement(file_get_contents(__DIR__.'/../Fixtures/last-commits.xml'));
+    }
+
+    protected function tearDown()
+    {
+        $this->xml = null;
+    }
+
+    public function testGetCommits()
+    {
+        $history = new CommitHistory($this->xml);
+        $commits = $history->getCommits();
+
+        $this->assertEquals(23, count($history->getCommits()));
+        $this->assertEquals(28, $commits[0]->getBuild());
+    }
+}
